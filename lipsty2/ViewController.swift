a15988e5fc1c0c285d532d6ef319db2d52604b23
Martin Ziserman <martinziserman@gmail.com>
2016-06-11 23:44:08 +0200

    Initial Commit

---
+++ lipsty2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lipsty2
+//
+//  Created by Martin Ziserman on 11/06/2016.
+//  Copyright © 2016 Martin Ziserman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
